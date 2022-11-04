b58c434e1567d07b30f9204c0ddc8f53eacf93a0
Eric Sun <ericmsun@gmail.com>
2022-11-04 18:48:28 -0400

    changed chrome extension name to Wiki-themes

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function replaceText(element) {
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
-        // element.textContent = element.textContent.replace(/Joe/gi, 'Bossmon')
-        document.querySelector('h1').innerText = 'JavaScript is amazing!';
+        element.textContent = element.textContent.replace(/Joe/gi, 'Bossmon');
+        // document.querySelector('h1').innerText = 'JavaScript is amazing!';
     }
 
 }
